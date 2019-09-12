dbf60ae37a391f190194526f6b39e14eb00dfa83
Ulrich Lukas <uli@domain.invalid>
2019-09-12 21:07:58 +0200

    better_str_to_f

--- u_sci_numbers.py
+++ u_sci_numbers.py
@@ -1,15 +1,13 @@
-import re
+def sane_str_to_f(s):
+    """Returns float from string input, agnostic to language-specific
+    decimal point format and with possible scientific number notation.
+    
+    Thousands separator not allowed.
 
-def validate_number(val):
-    if type(val) is str:
-        # Returns float from string input, taking the first occurrence of
-        # any comma /or/ dot character as only valid thousands separator
-        # and discards numbers following any non-number characters
-        m = re.match(r"(\d+[,.]?)(\d*)(([eE][+-]?\d+)?)", val)
-        if m is not None:
-            s = m.group(1).replace(",", ".") + m.group(2) + m.group(3)
-            return float(s)
-        else:
-            return float("NaN")
-    elif type(val) in (int, float):
-        return val
+    For invalid or empty input string, a float NaN value is returned.
+    """
+    try:
+        f = float(s.replace(",", "."))
+    except ValueError:
+        return float("NaN")
+    return f
